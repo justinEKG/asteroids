a37902a2ec4ffc804ff5d4a9868f125e6044fade
justinekg <jhartline@schwarzpartners.com>
2024-10-09 16:16:05 -0400

    added player rotation

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
             dt = clock.tick(60) / 1000
+            player.update(dt)
 
         screen.fill("black")
         pygame.display.flip()
