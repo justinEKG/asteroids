0bac5848d0c69de08e788f8f54e8c4be0c97ef73
justinekg <jhartline@schwarzpartners.com>
2024-10-09 15:37:09 -0400

    Added game clock to limit game to 60 FPS

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from constants import *
 
 def main():
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            dt = clock.tick(60) / 1000
 
-        screen = pygame.display.fill()
+        screen.fill("black")
         pygame.display.flip()
 
 
